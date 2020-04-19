29db3598e2592d2d93fc2a29608d318ad9274f53
restry <michael.wei@foxmail.com>
2020-04-19 23:24:34 +0800

    Update server.js

--- express/server.js
+++ express/server.js
@@ -56,5 +56,5 @@ app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 // app.use(fallback());
 
-module.exports = app;
+// module.exports = app;
 module.exports.handler = serverless(app);
