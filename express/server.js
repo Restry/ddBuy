674a7235adbbf7e0b2560906c1f4340c6d6e4107
Restry <michael.wei@foxmail.com>
2020-04-19 20:20:04 +0800

    update server slide api

--- express/server.js
+++ express/server.js
@@ -7,17 +7,20 @@ const app = express();
 const bodyParser = require('body-parser');
 // const api = require('./yuque');
 // const fallback = require('connect-history-api-fallback')
+const proxy = require('express-http-proxy');
 
 const router = express.Router();
-router.get('/', (req, res) => {
-  const { p } = req.query;
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  console.log(`query:${JSON.stringify(req.query)}`);
-  res.write(fs.readdirSync(path.resolve(__dirname, decodeURIComponent(p))).join(';'));
-  // res.sendFile(path.resolve(__dirname, '../dist/index.html'))
-  res.end();
-});
+// router.get('/', (req, res) => {
+//   const { p } = req.query;
+//   res.writeHead(200, { 'Content-Type': 'text/html' });
+//   console.log(`query:${JSON.stringify(req.query)}`);
+//   res.write(fs.readdirSync(path.resolve(__dirname, decodeURIComponent(p))).join(';'));
+//   // res.sendFile(path.resolve(__dirname, '../dist/index.html'))
+//   res.end();
+// });
 
+// 反向代理
+router.use('/', proxy('http://api.7-orange.cn:7300/mock/5def6a2d448e330a1116366e/api'));
 
 // router.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../dist/index.html')));
 // router.use('/api', api);
